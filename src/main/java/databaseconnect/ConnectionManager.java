b1ef6347928e4eaa322bfb6a1832fe9ad6da8413
johnngugi <johnngugi777@gmail.com>
2018-10-26 14:02:54 +0300

    Add new method

--- src/main/java/databaseconnect/ConnectionManager.java
+++ src/main/java/databaseconnect/ConnectionManager.java
@@ -4,28 +4,37 @@
  * and open the template in the editor.
  */
 package databaseconnect;
-import java.sql.*;
-import sun.applet.Main;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
- *
  * @author mathe
  */
 public class ConnectionManager {
 
     private static String url = "jdbc:mysql://localhost:3306/notifi_db";
     private static String driverName = "com.mysql.jdbc.Driver";
-    private static String username = "root";
-    private static String password = "";
+    private static String username = null;
+    private static String password = null;
     private static Connection con;
 
-    public static Connection getConnection() {
+    public Connection getConnection() {
         try {
             Class.forName(driverName);
             try {
+                getPropValues();
                 con = DriverManager.getConnection(url, username, password);
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection.");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         } catch (ClassNotFoundException ex) {
             // log an exception. for example:
@@ -34,7 +43,29 @@ public class ConnectionManager {
         System.out.println("Success");
         return con;
     }
-    
+
+    private void getPropValues() throws IOException {
+        InputStream inputStream = null;
+
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            username = prop.getProperty("username");
+            password = prop.getProperty("password");
+        } finally {
+            inputStream.close();
+        }
+    }
+
     public static void main(String[] args) {
         ConnectionManager conman = new ConnectionManager();
         conman.getConnection();
